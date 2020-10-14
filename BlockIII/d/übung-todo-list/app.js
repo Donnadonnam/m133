1d4c1e4f54be381b16f915a92412f592ab9fee0e
Yazdan Musa <yazdanmusa@gmail.com>
2020-10-14 11:17:23 +0200

    BLKIII: Todo List

---
+++ BlockIII/d/übung-todo-list/app.js
@@ -0,0 +1,20 @@
+function addToDoList(event) {
+  event.preventDefault();
+  var text = document.getElementById("input-field").value;
+  if (text == "")
+    return false;
+
+  text.
+
+  var ul = document.getElementById("list");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(text));
+  ul.appendChild(li);
+  document.getElementById("input-field").value = "";
+  return false;
+}
+
+
+
+var form = document.getElementById("input");
+form.addEventListener("submit", addToDoList);
