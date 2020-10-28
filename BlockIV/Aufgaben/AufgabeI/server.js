4dfd9e2559d23184ab767b8575c05d88d9ea75cf
Yazdan Musa <yazdanmusa@gmail.com>
2020-10-28 13:26:08 +0100

    BLKIV: Aufgabe I

--- BlockIV/Aufgaben/AufgabeI/server.js
+++ BlockIV/Aufgaben/AufgabeI/server.js
@@ -1,6 +1,20 @@
-import { serve } from "https://deno.land/std@0.74.0/http/server.ts";
-const s = serve({port: 8000});
-console.log("http://127.0.0.1:8000/");
-for await (const req of s){
-    req.respond({body: "Hello World\n"});
+import {
+  serve
+} from "https://deno.land/std@0.74.0/http/server.ts";
+const server = serve({
+  port: 8000
+});
+console.log("HTTP Web Server running: http://127.0.0.1:8000/");
+
+for await (const request of server) {
+  const index = await Deno.open("index.html");
+  if (request.url == "/bar") {
+    request.respond({
+      body: index
+    });
+  }
+  request.respond({
+    status: 200,
+    body: index
+  });
 }
